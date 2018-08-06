4311ecb4638ff68373d88f8cd07423b8f2694cd4
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-08-06 16:26:42 +0300

    Php variables

--- site.php
+++ site.php
@@ -4,13 +4,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>PHP App</title>
 </head>
 <body>
     
     <?php
-        echo("Hello World");
-        echo "<h1>Sivar Site</h1>";
+        $characterName = "Sivar Sarkawt";
+        $characterAge = 23;
+        echo "My name is $characterName and I'm $characterAge years old <br>";
+
+        $characterName = "Stealth";
+        $characterAge = 24;
+        echo "My name is $characterName and I'm $characterAge years old";
+
+        // echo("Hello World");
+        // echo "<h1>Sivar Site</h1>";
+        // echo "<hr>";
+        // echo "<p>This is my site</p>";
     ?>
 
 </body>
