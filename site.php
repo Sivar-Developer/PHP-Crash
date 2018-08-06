e5f5dcd6f62a147f68945584401ca9fa448b66a0
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-08-06 16:51:52 +0300

    Working with Strings

--- site.php
+++ site.php
@@ -24,11 +24,24 @@
         // echo "<p>This is my site</p>";
 
         // Data Types
-        $phrase = "To be or not to be";     // String
-        $age = 23;                          // Iteger
-        $gpa = 32.45363;                    // Float
-        $isMale = false;                    // Boolean
+        $phrase = "To be or not to be <br>";    // String
+        $age = 23;                              // Iteger
+        $gpa = 32.45363;                        // Float
+        $isMale = false;                        // Boolean
         echo $phrase;
+
+        // Working with strings
+        $phrase = "Sivar Developer <br>";
+        $phrase[0] = "B";                       // change index char of string
+        echo strtolower($phrase);               // Lower case chars
+        echo strtoupper($phrase);               // Upper case chars
+        echo strlen($phrase);                   // Length of String
+        echo "<br>";
+        echo $phrase[0];                        // String Index
+        echo "Mike"[1];
+        echo "<br>";
+        echo str_replace("Bivar", "Stealth", $phrase);  // Replace String characters
+        echo substr($phrase, 6, 7);             // Return part of a string
     ?>
 
 </body>
