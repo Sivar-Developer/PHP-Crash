3e667ab6cd2b7d6dc6aa3deae9cb45fbec481e76
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-08-07 00:23:13 +0300

    Using checkboxes

--- site.php
+++ site.php
@@ -141,6 +141,18 @@
         // echo $friends[1];
         // echo $friends[2];
         // echo $friends[3];
+    ?><hr>
+
+    <!-- Using checkboxes -->
+    <form action="site.php" method="post">
+        Apples: <input type="checkbox" name="fruits[]" value="apples"><br>
+        Oranges: <input type="checkbox" name="fruits[]" value="oranges"><br>
+        Pears: <input type="checkbox" name="fruits[]" value="pears"><br>
+        <input type="submit">
+    </form>
+    <?php
+        $fruits = $_POST["fruits"];
+        echo $fruits[0];
     ?>
 
 </body>
