ab39235ab96f41a234a7f2e8286d84940b28fa3e
Sivar Sarkawt <sivar.sarkawt@engineer.com>
2018-08-06 16:34:00 +0300

    Data Types

--- site.php
+++ site.php
@@ -9,18 +9,26 @@
 <body>
     
     <?php
+        // Variables
         $characterName = "Sivar Sarkawt";
         $characterAge = 23;
         echo "My name is $characterName and I'm $characterAge years old <br>";
 
         $characterName = "Stealth";
         $characterAge = 24;
-        echo "My name is $characterName and I'm $characterAge years old";
+        echo "My name is $characterName and I'm $characterAge years old <br><hr>";
 
         // echo("Hello World");
         // echo "<h1>Sivar Site</h1>";
         // echo "<hr>";
         // echo "<p>This is my site</p>";
+
+        // Data Types
+        $phrase = "To be or not to be";     // String
+        $age = 23;                          // Iteger
+        $gpa = 32.45363;                    // Float
+        $isMale = false;                    // Boolean
+        echo $phrase;
     ?>
 
 </body>
